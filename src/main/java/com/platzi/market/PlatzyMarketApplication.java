49db284c0b0e9fb689b6bb26ada76808bea798fe
Agus Ibaceta <aibaceta16@gmail.com>
2021-03-19 03:44:20 -0300

    Inizializated project and created base Entities

---
+++ src/main/java/com/platzi/market/PlatzyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzyMarketApplication.class, args);
+	}
+
+}
